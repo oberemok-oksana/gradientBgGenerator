7fc1759ff3c9bf3b16a80569eac8ba742dd3ccd7
Oksana Oberemok <Pylypenkok@gmail.com>
2023-02-03 21:26:53 +0700

    add eventListener to copyBtn

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const firstColorInput = document.querySelector(".color1");
 const secondColorInput = document.querySelector(".color2");
 const body = document.getElementById("root");
 const rootCss = document.querySelector(":root");
+const copyBtn = document.querySelector(".copy-btn");
 
 function setGradient() {
   rootCss.style.setProperty("--beginGradient", firstColorInput.value);
   rootCss.style.setProperty("--endGradient", secondColorInput.value);
+  typeGradientProperties();
+}
+
+function typeGradientProperties() {
   const indexOfRepeat = getComputedStyle(document.body)
     .getPropertyValue("background")
     .indexOf("repeat");
@@ -18,3 +23,11 @@ function setGradient() {
 firstColorInput.addEventListener("input", setGradient);
 
 secondColorInput.addEventListener("input", setGradient);
+
+copyBtn.addEventListener("click", () => {
+  if (gradientContainer.textContent) {
+    navigator.clipboard.writeText(gradientContainer.textContent);
+    const tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied";
+  }
+});
